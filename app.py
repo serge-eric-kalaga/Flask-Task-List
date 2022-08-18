41fa6939a147a1159e75e753568c9160d8eb7c0a
serge-eric-kalaga <kalagaserge4@gmail.com>
2022-08-18 11:18:42 +0200

    reglage version 2 ok

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class User(db.Model):
 class Task(db.Model):
        id = db.Column(db.Integer, primary_key=True)
        title = db.Column(db.String(255), nullable=False)
-       description = db.Column(db.Text, nullable=True)
+       description = db.Column(db.Text, nullable=True, default="No description")
        date = db.Column(db.DateTime, default=datetime.now)
        user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
        
